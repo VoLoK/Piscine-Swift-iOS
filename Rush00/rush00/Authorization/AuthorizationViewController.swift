e7d8360f5044f5ff275edeb0618bc372cda73334
Anisa Kapateva <anisa56695@gmail.com>
2022-08-17 21:59:40 +0300

    AuthorizationViewController

--- Rush00/rush00/Authorization/AuthorizationViewController.swift
+++ Rush00/rush00/Authorization/AuthorizationViewController.swift
@@ -7,23 +7,138 @@
 
 import Foundation
 import UIKit
+import WebKit
 
-class AuthorizationViewController: UIViewController {
+enum EState {
+    case code
+    case token
+}
+
+class AuthorizationViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+    
+    var webView: WKWebView!
+    var state: String = ""
+    var current_state: EState = EState.code
+    var storeToken: [String: String] = [:]
+    var err: Error!
+    
+    override func loadView() {
+        let dataStore = WKWebsiteDataStore.default()
+        dataStore.fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { (records) in
+            for record in records {
+                if record.displayName.contains("42") {
+                    dataStore.removeData(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), for: [record], completionHandler: {
+                        DispatchQueue.main.async {
+                            self.webView.reload()
+                        }
+                    })
+                }
+            }
+        }
+        webView = WKWebView()
+        self.webView.uiDelegate = self
+        self.webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .red
+        view.backgroundColor = .lightGray
         title = "Welcome"
-        let logOutButton = UIBarButtonItem(title: "Log Out",
-                                           style: .done,
-                                           target: self,
-                                           action: #selector(popVC(sender:)))
-        navigationItem.rightBarButtonItem = logOutButton
+        authorization()
     }
 }
 
 extension AuthorizationViewController {
-    @objc func popVC(sender: UIBarButtonItem) {
-        navigationController?.popViewController(animated: true)
+    func randomString(length: Int) -> String {
+      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+    
+    func tryLogin() {
+        // Переход на экран UserProfileViewController
+//        let controller = UserProfileViewController(with: UserProfileViewModel()) // вся модель
+//        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    func authorization() {
+        self.state = randomString(length: 20)
+        let base_url = "https://api.intra.42.fr/oauth/authorize"
+
+        let parametrs: [String : String] = [
+            "client_id" : TokenService.uid,
+            "redirect_uri" : "http://127.0.0.1",
+            "scope" : "public+profile",
+            "state" : self.state,
+            "response_type" : "code"
+        ]
+        var substr: String = ""
+        for (key, val) in parametrs {
+            substr += "&\(key)=\(val)"
+        }
+        let requestStr = base_url + "?" + substr
+        let url = URL(string: requestStr)
+        var request: URLRequest = URLRequest(url: url!)
+        request.httpMethod = "GET"
+        webView.load(request)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let components = URLComponents(string: navigationAction.request.url!.absoluteString)
+        let code = components?.queryItems?.first { $0.name == "code" }?.value
+        let state = components?.queryItems?.first { $0.name == "state" }?.value
+        if navigationAction.request.url!.absoluteString.contains("error=access_denied") {
+            self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+            decisionHandler(WKNavigationActionPolicy.cancel)
+            return
+        }
+        
+        if code != nil && state == self.state && self.current_state == EState.code {
+            self.current_state = EState.token
+            let params: [String: String] = [
+                "client_id": TokenService.uid,
+                "client_secret": TokenService.secret,
+                "redirect_uri": "http://127.0.0.1",
+                "state": self.state,
+                "code": code!,
+                "grant_type": "authorization_code",
+            ]
+            let urlString = "https://api.intra.42.fr/oauth/token"
+            var substr: String = ""
+            for (key, val) in params {
+                substr += "&\(key)=\(val)"
+            }
+            let url: URL = URL(string: urlString)!
+            var request: URLRequest = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = substr.data(using: String.Encoding.utf8)
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let err = error {
+                    self.err = err
+                }
+                else if let d = data {
+                    do {
+                        if let dic : NSDictionary = try JSONSerialization.jsonObject(with: d) as? NSDictionary {
+                            self.storeToken["token"] = dic.value(forKey: "access_token") as? String
+                            DispatchQueue.main.async() {
+                                print(self.storeToken)
+                                user_token = self.storeToken["token"] ?? "0"
+                                print(user_token)
+                                self.tryLogin()
+                            }
+                        }
+                    }
+                    catch (let err) {
+                        self.err = err
+                    }
+                }
+            }
+            task.resume()
+        }
+        decisionHandler(WKNavigationActionPolicy.allow)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        decisionHandler(WKNavigationResponsePolicy.allow)
     }
 }
--- Rush00/rush00/TokenService/TokenService.swift
+++ Rush00/rush00/TokenService/TokenService.swift
@@ -11,3 +11,5 @@ struct TokenService {
     static var uid = "f653c60068f895e559efc7837ba5ad8508b869ff02f1e54525cc7fc5a72481d7"
     static var secret = "c38c082b749ca5b785c9b5084883309e62e5d126c89bbdeeef5eab795e598849"
 }
+
+var user_token: String = ""
